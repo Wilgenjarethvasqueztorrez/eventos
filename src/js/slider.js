c39de3aef144b44e9a5a64d69c0f5877500d9374
wilgen jareth vasquez torrez <est.wvasquezt0201@uml.edu.ni>
2023-04-22 11:13:55 -0600

    Despliege

--- src/js/slider.js
+++ src/js/slider.js
@@ -6,23 +6,23 @@ slider.innerHTML = `
 <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
      <!-- Item 1 -->
     <div class="hidden duration-700 ease-in-out" data-carousel-item="active">
-        <img src="/src/imagenes/1.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+        <img src="./imagenes/carousel/1.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
     </div>
     <!-- Item 2 -->
     <div class="hidden duration-700 ease-in-out" data-carousel-item>
-        <img src="src/imagenes/2.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+        <img src="./imagenes/carousel/2.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
     </div>
     <!-- Item 3 -->
     <div class="hidden duration-700 ease-in-out" data-carousel-item>
-        <img src="src/imagenes/3.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+        <img src="./imagenes/carousel/3.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
     </div>
     <!-- Item 4 -->
     <div class="hidden duration-700 ease-in-out" data-carousel-item>
-        <img src="src/imagenes/4.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+        <img src="./imagenes/carousel/4.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
     </div>
     <!-- Item 5 -->
     <div class="hidden duration-700 ease-in-out" data-carousel-item>
-        <img src="src/imagenes/5.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+        <img src="./imagenes/carousel/5.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
     </div>
 </div>
 <!-- Slider indicators -->
